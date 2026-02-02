b5afadc4080705bfa425634b6fb10532c439c439
mehmettguzell <>
2026-02-02 15:57:32 +0300

    .

--- services/product-service/products/models.py
+++ services/product-service/products/models.py
@@ -1,3 +1,16 @@
+import uuid
 from django.db import models
 
 # Create your models here.
+class Product(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    sku = models.CharField(max_length=50, unique=True)
+    name = models.CharField(max_length=255)
+    description = models.TextField(blank=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    is_active = models.BooleanField(default=True)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.sku} - {self.name}"
--- services/user-service/users/models.py
+++ services/user-service/users/models.py
@@ -1,3 +1,13 @@
+import uuid
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+class User(AbstractUser):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    email = models.EmailField(unique=True)
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = ["username"]  # superuser için gerekli
+
+    def __str__(self):
+        return self.email
